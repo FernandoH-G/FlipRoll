5cfeae806b4f79f464cdc09a2ab784dece429cee
Fernando <fherrera2018@gmail.com>
2021-01-02 20:01:58 -0800

    Modified .gitignore

---
+++ app/src/main/java/com/fhg/fliproll/room/FlipRollRoomDatabase.kt
@@ -0,0 +1,4 @@
+package com.fhg.fliproll.room
+
+class FlipRollRoomDatabase {
+}
---
+++ app/src/main/java/com/fhg/fliproll/viewmodel/FlipRollViewModel.kt
@@ -0,0 +1,6 @@
+package com.fhg.fliproll.viewmodel
+
+import androidx.lifecycle.ViewModel
+
+class FlipRollViewModel : ViewModel() {
+}
